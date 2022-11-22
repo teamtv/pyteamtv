0067d29f9ba40eb8768bb2abfc2542aba736a7e6
Koen Vossen <info@koenvossen.nl>
2022-11-22 17:03:57 +0100

    Update example

--- examples/fetch_tags.py
+++ examples/fetch_tags.py
@@ -3,6 +3,7 @@ from os import environ
 from dotenv import load_dotenv
 
 from pyteamtv import TeamTVUser
+from pyteamtv.dataframe_builder import DataframeBuilder
 
 
 def main():
@@ -14,11 +15,14 @@ def main():
     api = TeamTVUser(jwt_token=environ["TEAMTV_API_TOKEN"])
     team = api.get_team(team_name)
 
+    builder = DataframeBuilder(team)
+
     sporting_event = team.get_sporting_event(sporting_event_id)
     video = sporting_event.get_videos_by_tags(output_key="switched")[0]
+    df = builder.build_df([
+        sporting_event.get_observation_log(video_id=video.video_id)
+    ])
 
-    observation_log = sporting_event.get_observation_log(video_id=video.video_id)
-    df = observation_log.to_pandas().to_dict(orient="records")
     print(df)
 
 
